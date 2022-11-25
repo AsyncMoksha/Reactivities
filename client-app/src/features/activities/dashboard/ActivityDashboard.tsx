5dfa8691a03d0ca12def7f76823c1dc35217e1fd
Zinan Xing <z@xinglab.com>
2022-11-25 16:38:03 -0700

    debug

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,11 +15,12 @@ interface Props {
   closeForm: () => void;
   createOrEdit: (activity: Activity) => void;
   deleteActivity: (id: string) => void;
+  submitting: boolean;
 }
 
 export default function ActivityDashboard(
   {activities, selectedActivity, selectActivity, cancelSelectActivity, 
-    editMode, openForm, closeForm, createOrEdit, deleteActivity} : Props) {
+    editMode, openForm, closeForm, createOrEdit, deleteActivity, submitting} : Props) {
   return (
     <Grid>
       <Grid.Column width='10'>
@@ -36,7 +37,11 @@ export default function ActivityDashboard(
           openForm={openForm}
         />}
         {editMode && 
-        <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />}
+        <ActivityForm 
+          closeForm={closeForm} 
+          activity={selectedActivity} 
+          createOrEdit={createOrEdit} 
+          submitting={submitting} />}
       </Grid.Column>
     </Grid>
   );
