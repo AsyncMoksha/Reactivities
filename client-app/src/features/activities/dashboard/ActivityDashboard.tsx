7c632ca5333bcf49053142fa9dfabf5b4a3d4c6c
Zinan Xing <z@xinglab.com>
2022-11-28 18:34:49 -0700

    end of section 6

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -25,9 +25,10 @@ export default function ActivityDashboard(
     <Grid>
       <Grid.Column width='10'>
         <ActivityList 
-        activities={activities} 
-        selectActivity={selectActivity}
-        deleteActivity={deleteActivity} />
+          activities={activities} 
+          selectActivity={selectActivity}
+          deleteActivity={deleteActivity}
+          submitting={submitting} />
       </Grid.Column>
       <Grid.Column width='6'>
         {selectedActivity && !editMode && 
